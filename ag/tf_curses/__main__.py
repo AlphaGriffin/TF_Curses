885f790138de2c31849e3fcba141763c1383fe74
Eric Petersen <eric.alphagriffin@gmail.com>
2017-03-11 03:48:07 -0700

    first hello world tutorial... progress!

--- ag/tf_curses/__main__.py
+++ ag/tf_curses/__main__.py
@@ -9,18 +9,16 @@ Alphagriffin.com
 Eric Petersen @Ruckusist <eric.alphagriffin@gmail.com>
 """
 import sys
+import urwid
 
 
-def main(args=None):
+def main(args):
     """The main routine."""
-    if args is None:
-        args = sys.argv[1:]
-
-    print("This is the main routine.")
-    print("It should do something interesting.")
-
-    # Do argument parsing here (eg. with argparse) and anything else
-    # you want your project to do.
+    # tutorial beginnings
+    txt = urwid.Text("Hello World")
+    fill = urwid.Filler(txt, 'top')
+    loop = urwid.MainLoop(fill)
+    loop.run()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
---
+++ launch.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+import ag.tf_curses.__main__ as app
+if __name__ == '__main__':
+    try:
+        app.main(sys.argv)
+    except:
+        print("Thanks for using Alphagriffin.com")
